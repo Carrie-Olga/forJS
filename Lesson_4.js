7ab20a76c7191826b5ca5356a08dd86c574bd871
Carrie-Olga <morgunova@protonmail.com>
2019-08-04 14:55:42 +0200

    Lesson 4 task 1

--- Lesson_4.js
+++ Lesson_4.js
@@ -4,22 +4,27 @@
 //мы должны получить следующий объект: {‘единицы’: 5, ‘десятки’: 4, ‘сотни’: 2}. Если число превышает 999, 
 //необходимо выдать соответствующее сообщение с помощью console.log и вернуть пустой объект.
 
-const numToObj = (num) => {
-    if (num < 0 || num > 999) {
-        console.log('Input correct number');
-        return Object;
-    }
-    var str = num.toString();
-    console.log(str);   
-    var arr = new Array();
-    arr = str.split('');
-    //return arr;
-    var obj = {
-        единицы: Number(arr[2]),
-        десятки: Number(arr[1]),
-        сотни: Number(arr[0]) 
-    };
-    return obj;
-}
+    // const numToObj = (num) => {
+    //     if (num < 0 || num > 999) {
+    //         console.log('Input correct number');
+    //         return Object;
+    //     }
+    //     var str = num.toString();
+    //     console.log(str);   
+    //     var arr = new Array();
+    //     arr = str.split('');
+    //     //return arr;
+    //     var obj = {
+    //         единицы: Number(arr[2]),
+    //         десятки: Number(arr[1]),
+    //         сотни: Number(arr[0]) 
+    //     };
+    //     return obj;
+    // }
 
-console.log(numToObj(199));
+    // console.log(numToObj(199));
+
+//2. Для игры, реализованной на уроке, добавить возможность вывода хода номер n (номер задается пользователем)
+
+
+
