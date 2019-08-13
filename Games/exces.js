381cf6ac0c052891c920a87f3143fe0ca5fed40a
Carrie-Olga <morgunova@protonmail.com>
2019-08-13 14:43:51 +0100

    крестиик нолики логика сделана

--- Games/exces.js
+++ Games/exces.js
@@ -1,23 +1,44 @@
-const ex = "X";
-const nil = "O";
 var player = true;
-
 var cells = document.querySelectorAll('.cell');
+const board = document.querySelector('.board');
 
 cells.forEach(cell => {
     cell.addEventListener('click', showSymbol);
-    //player != player;
-})
+});
 
 function showSymbol(event) {
-
     var cell = event.target;
     cell.classList.add('checked');
-    // if (player) {
-    //     cell.innerHTML = ex;
-    // } else {
-    //     cell.innerHTML = nil;
-    // }
-    cell.innerText = player ? ex : nil;
+    cell.innerText = player ? 'X' : 'O';
     player = !player;
-}
+    checkWinner();
+};
+
+function checkWinner() {
+    if ((cells[0].innerHTML == 'X' && cells[1].innerHTML == 'X' && cells[2].innerHTML == 'X') || 
+        (cells[3].innerHTML == 'X' && cells[4].innerHTML == 'X' && cells[5].innerHTML == 'X') ||
+        (cells[6].innerHTML == 'X' && cells[7].innerHTML == 'X' && cells[8].innerHTML == 'X') ||
+        (cells[0].innerHTML == 'X' && cells[3].innerHTML == 'X' && cells[6].innerHTML == 'X') ||
+        (cells[1].innerHTML == 'X' && cells[4].innerHTML == 'X' && cells[7].innerHTML == 'X') ||
+        (cells[2].innerHTML == 'X' && cells[5].innerHTML == 'X' && cells[8].innerHTML == 'X') ||
+        (cells[2].innerHTML == 'X' && cells[4].innerHTML == 'X' && cells[6].innerHTML == 'X') ||
+        (cells[0].innerHTML == 'X' && cells[4].innerHTML == 'X' && cells[8].innerHTML == 'X') ) {
+            var winner = document.createElement('div');
+            winner.innerText = 'X won!';
+            board.innerHTML = winner;
+            console.log(winner);
+        } else if ((cells[0].innerHTML == 'O' && cells[1].innerHTML == 'O' && cells[2].innerHTML == 'O') || 
+        (cells[3].innerHTML == 'O' && cells[4].innerHTML == 'O' && cells[5].innerHTML == 'O') ||
+        (cells[6].innerHTML == 'O' && cells[7].innerHTML == 'O' && cells[8].innerHTML == 'O') ||
+        (cells[0].innerHTML == 'O' && cells[3].innerHTML == 'O' && cells[6].innerHTML == 'O') ||
+        (cells[1].innerHTML == 'O' && cells[4].innerHTML == 'O' && cells[7].innerHTML == 'O') ||
+        (cells[2].innerHTML == 'O' && cells[5].innerHTML == 'O' && cells[8].innerHTML == 'O') ||
+        (cells[2].innerHTML == 'O' && cells[4].innerHTML == 'O' && cells[6].innerHTML == 'O') ||
+        (cells[0].innerHTML == 'O' && cells[4].innerHTML == 'O' && cells[8].innerHTML == 'O') ) {
+            var winner = document.createElement('div');
+            winner.innerText = 'O won!';
+            board.innerHTML = winner;
+            console.log(winner);
+        } 
+};
+
